ce293a5ac4b19e3122d92cb89a0a9e67a87f30f9
WMD11 <113972186+WMD11@users.noreply.github.com>
2023-11-22 23:16:32 +0200

    Update 02-video.js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -4,22 +4,25 @@ import throttle from 'lodash.throttle';
 const iframe = document.querySelector('#vimeo-player');
 const player = new Player(iframe);
 
-
+// Definește funcția onTimeUpdate
 const onTimeUpdate = throttle((data) => {
     localStorage.setItem('videoplayer-current-time', data.seconds);
 }, 1000);
 
-var throttle = require('lodash.throttle');
+// Ascultă evenimentul 'timeupdate'
 player.on('timeupdate', onTimeUpdate);
 
-const savedTime = localStorage.getItem('videoplayer-current-time') || 0;
-player.setCurrentTime(savedTime).catch((error) => {
-    switch (error.name) {
-        case 'RangeError':
-           
-            break;
-        default:
-          
-            break;
-    }
+// Așteaptă ca playerul să fie pregătit înainte de a seta timpul curent
+player.ready().then(() => {
+    const savedTime = localStorage.getItem('videoplayer-current-time') || 0;
+    player.setCurrentTime(savedTime).catch((error) => {
+        switch (error.name) {
+            case 'RangeError':
+                // Gestionează RangeError
+                break;
+            default:
+                // Gestionează alte erori
+                break;
+        }
+    });
 });
